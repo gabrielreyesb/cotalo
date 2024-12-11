badd3d5a345abc336cc16869a092fcbab9b3c2a3
Gabriel Reyes <gabriel@handy.la>
2024-12-11 15:48:11 -0600

    Heroku

--- app/javascript/controllers/turbo_frame_controller.js
+++ app/javascript/controllers/turbo_frame_controller.js
@@ -1,19 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = {
-    url: String
-  }
-
   connect() {
-    // Remove or comment out the console.log
-    // console.log("TurboFrame controller connected", this.urlValue)
+    // Add any initialization logic here
   }
-
-  navigate() {
-    console.log("Navigate called with URL:", this.urlValue)
-    if (this.urlValue) {
-      window.Turbo.visit(this.urlValue)
-    }
-  }
-} 
+}
