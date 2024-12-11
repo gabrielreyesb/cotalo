42562dce7fd41808eeed5a208007a4448e707f91
Gabriel Reyes <gabriel@handy.la>
2024-12-11 15:55:06 -0600

    Heroku

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "controllers/application", to: "controllers/application.js"
+pin "controllers/hello_controller", to: "controllers/hello_controller.js"
+pin "controllers/turbo_frame_controller", to: "controllers/turbo_frame_controller.js"
+pin "controllers", to: "controllers/index.js"
