64b3e408ded32b4d5cf013acc850e0c188d0e65b
Gabriel Reyes <gabriel@handy.la>
2024-12-11 16:31:12 -0600

    Remove collapse icon controller

--- app/javascript/controllers/index.js
+++
@@ -1,7 +0,0 @@
-import { application } from "@hotwired/stimulus"
-import { registerControllers } from "@hotwired/stimulus-loading"
-
-// Register all controllers in the controllers directory
-registerControllers(application, import.meta.globEager("./**/*_controller.js"))
-
-export { application }
