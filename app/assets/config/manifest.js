ce700177ff03c5c907a5262c6795d49ff83c6dfd
Gabriel Reyes <gabriel@handy.la>
2025-02-21 17:43:59 -0600

    Se agregan validaciones de datos completos en cotización

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link application.scss
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link sweetalert2/dist/sweetalert2.all.js
