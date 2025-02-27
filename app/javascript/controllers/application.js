fbdfef6213627b4904981f96e3c1a95ea25145c2
Gabriel Reyes <gabriel@handy.la>
2025-02-27 11:53:11 -0600

    Se agregan tooltips en home

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,15 @@
 import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
+
+// Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+// Initialize Bootstrap tooltips
+document.addEventListener("turbo:load", function() {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})
 
 export { application } 
